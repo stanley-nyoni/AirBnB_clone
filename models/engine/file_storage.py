fc2bd29c50bb39b93df7969b313be1cc7b589094
Nokuphila <nnblb58@gmail.com>
2023-10-12 10:07:23 -0700

    initial commit for classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,29 +7,31 @@ import os
 
 class FileStorage:
     """Defines a class for storing and retrieving data"""
+
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Function that returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
-    def new(self):
+    def new(self, obj):
         """Function to set in __object with key <obj.__class__.__name__, obj.id>"""
         if obj:
-            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-            self.__objects[key] = obj
+            key = '{}.{}'.format(type(obj).__class__.__name__, obj.id)
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """Function to save/serialize object to json file"""
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            dic = {key: value.to_dict() for key, value in self.__objects.items()}
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             json.dump(dic, f)
 
     def reload(self):
         """Function to reload saved objects"""
-        with open(self.__file_path, "r", encoding="utf-8") as f:
-            new_obj_dict = json.load(f)
-        for key, value in new_obj_dict.items():
-            obj = self.classes()[value["__class__"]](**value)
-            self.__objects[key] = obj
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                new_obj = json.load(f)
+                FileStorage.__objects = new_obj
+        except FileNotFoundError:
+            pass
